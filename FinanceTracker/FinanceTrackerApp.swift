732e14217ff9291479fbedfc8c8461474401f4f7
Doolot Yndybaev <91968210+DoolotYndybaev@users.noreply.github.com>
2025-06-09 18:15:51 +0600

    Initial Commit

---
+++ FinanceTracker/FinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerApp.swift
+//  FinanceTracker
+//
+//  Created by Doolot on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
